d97e72c8c62c342bb0b9e271361003ff86b53bf2
brentnettles <brentnettles@gmail.com>
2024-01-29 22:49:58 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 100) +1;
+
+//"Mod" = "The remainder" need remainder equal to 4
+const num3 = 25
+const num4 = 7
+const mod =  num3 % num4; 
+
+//without the Spread syntax (...before numberList I was getting the error: expected NaN to equal 20)
+const numberList = [0,5,10,15,20];
+const max = Math.max(...numberList);
+
